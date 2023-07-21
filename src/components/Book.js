71320f32cabbdad7b95a01a1319ac19bcce6d26d
John Murianki <mcjthiongo@gmail.com>
2023-07-22 01:27:47 +0300

    Fix linter errors

--- src/components/Book.js
+++ src/components/Book.js
@@ -10,13 +10,11 @@ function Book({ bookProp, id }) {
   const ranNumber = Math.floor(Math.random() * 10) + 1;
   const [error, setError] = useState(null);
 
-
   const handleRemoveBook = async () => {
     try {
       await dispatch(RemoveBook(id));
       await dispatch(FetchBooks());
     } catch (error) {
-
       setError('Error removing book. Please try again later.');
     }
   };
